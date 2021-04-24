b521f82f650b8702a8a39a59be3449f611e1c67b
ChrisLozano86 <chris.lozano8603@gmail.com>
2021-04-23 19:52:37 -0500

    23/04/2021

--- dashboard/modules/travel_agencies/export_report.php
+++ dashboard/modules/travel_agencies/export_report.php
@@ -1,3 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1 " />
+    <title>Exportar Reporte</title>
+</head>
+<body>
+    
 <?php
 require_once ('../../class/Agencia.php');
 $agencia = Agencia::recuperarTodos();
@@ -11,9 +23,13 @@ foreach($agencia as $item){
 
 }
 $salida .= "</table>";
+header("Content-Type: application/vnd.ms-excel charset=iso-8859-1");
 header("Content-type: application/vnd.ms-excel");
 header("Content-Disposition: attachment; filename=Holiday_Report_".time().".xls");
 header("Pragma: no-cache");
 header("Expires: 0");
 echo $salida;
 ?>
+
+</body>
+</html>
