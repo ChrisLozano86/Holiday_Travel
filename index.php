59f474dc250d45f52a01d50385e2e9aac679213d
ChrisLozano86 <chris.lozano8603@gmail.com>
2021-05-08 11:20:06 -0500

    08/05/2021

--- index.php
+++ index.php
@@ -1,95 +1,4 @@
-
-<?php
-require_once './dashboard/class/Agencia.php';
-
-    $idAgencia = (isset($_REQUEST['idAgencia'])) ? $_REQUEST['idAgencia'] : null;
-    
-    if($idAgencia){        
-        $agencia = Agencia::buscarPorId($idAgencia);        
-    }else{
-        $agencia = new Agencia();
-    }
-
-    //Request
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-        $agencia->setRazonSocial($_POST['razon_social']);
-        $agencia->setNombreComercial($_POST['nombre_comercial']);
-        $agencia->setRFC($_POST['rfc']);
-        $agencia->setCalle($_POST['calle']);
-        $agencia->setNumExterior($_POST['num_exterior']);
-        $agencia->setNumInterior($_POST['num_interior']);
-        $agencia->setColonia($_POST['colonia']);
-        $agencia->setMunicipio($_POST['municipio']);
-        $agencia->setCiudad($_POST['ciudad']);
-        $agencia->setEstado($_POST['estado']);
-        $agencia->setCP($_POST['cp']);
-        $agencia->setPais($_POST['pais']);
-        $agencia->setMoneda($_POST['moneda']);
-        $agencia->setTel1($_POST['tel1']);
-        $agencia->setTel2($_POST['tel2']);
-        $agencia->setTel3($_POST['tel3']);
-        $agencia->setPaginaWeb($_POST['pagina_web']);
-        $agencia->setActivo('Si');
-        $agencia->setClaveBackOffice($_POST['clave_back_office']);
-        $agencia->setHeaderFooter('Activo');
-        $agencia->setMenu('Activo');
-        //$agencia->setObservaciones($_POST['observaciones']);
-        $agencia->setNombreContacto($_POST['nombre_contacto']);
-        $agencia->setApellidoPaterno($_POST['apellido_paterno']);
-        $agencia->setApellidoMaterno($_POST['apellido_materno']);
-        $agencia->setCargo($_POST['cargo']);
-        $agencia->setSexo($_POST['sexo']);
-        $agencia->setTelDirecto($_POST['tel_directo']);
-        $agencia->setTelMovil($_POST['tel_movil']);
-        $agencia->setEmailContacto($_POST['email_contacto']);
-        $agencia->setEmailServidor($_POST['email_servidor']);
-        $agencia->setClave($_POST['clave']);
-        $agencia->setServidorSMTP($_POST['servidor_smtp']);
-        $agencia->setPortSMTP($_POST['port_smtp']);
-        $agencia->setFechaCreacion(date('Y-m-d'));
-
-
-        $rutaServidor = 'uploads/images';
-        $rutaServidorFiles = 'uploads/files';
-
-        if ($_FILES['url_img1']['name']!=null) {
-    
-          if (!is_dir('uploads/images')) {
-            mkdir('uploads/images', 0777, true); 
-          }
-         
-          $rutaTemporal1 = $_FILES['url_img1']['tmp_name'];
-          $extension = pathinfo($_FILES['url_img1']['name'], PATHINFO_EXTENSION);
-          $nombreImagen1 = date('YmdHis').'_logo.'.$extension;
-          $rutaDestino1 = $rutaServidor.'/'.$nombreImagen1;
-          unlink($_POST['logo']);
-          move_uploaded_file($rutaTemporal1, $rutaDestino1); 
-          $agencia->setLogo($rutaDestino1); 
-        
-    } else{
-          $agencia->setLogo($_POST['logo']);    
-  } 
-
-
-        $agencia->guardar();
-       
-        if($idAgencia != ""){
-            header('Location: index.php?status_code=2');
-        }else{
-            
-            //header('Location: index.php?status_code=1');
-            echo '<script>
-            alert("Se Guardó Correctamente");
-            
-            window.location.href="index.php";
-            </script>';
-
-        }
-        
-    }
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="es">
 
 <head>
@@ -456,7 +365,7 @@ require_once './dashboard/class/Agencia.php';
 
             <?php
             include_once './dashboard/class/Promocion.php';
-            $promo = Promocion::recuperarTodos();
+            $promo = Promocion::recuperarPromocionesPublicadas();
 
             if (count($promo) > 0) :
             ?>
@@ -532,7 +441,7 @@ require_once './dashboard/class/Agencia.php';
                                     </button>
                                 </div>
                                 <div class="modal-body">
-                                    <form action="index.php" method="post" id="slider_form" enctype="multipart/form-data">
+                                    <form action="lib/registrar_agencia.php" method="post" id="slider_form" enctype="multipart/form-data">
 
 
                                         <h4 class="section-form">Información</h4>
---
+++ lib/registrar_agencia.php
@@ -0,0 +1,143 @@
+<?php
+require_once '../dashboard/class/Agencia.php';
+
+    $idAgencia = (isset($_REQUEST['idAgencia'])) ? $_REQUEST['idAgencia'] : null;
+    
+    if($idAgencia){        
+        $agencia = Agencia::buscarPorId($idAgencia);        
+    }else{
+        $agencia = new Agencia();
+    }
+
+    //Request
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        $agencia->setRazonSocial($_POST['razon_social']);
+        $agencia->setNombreComercial($_POST['nombre_comercial']);
+        $agencia->setRFC($_POST['rfc']);
+        $agencia->setCalle($_POST['calle']);
+        $agencia->setNumExterior($_POST['num_exterior']);
+        $agencia->setNumInterior($_POST['num_interior']);
+        $agencia->setColonia($_POST['colonia']);
+        $agencia->setMunicipio($_POST['municipio']);
+        $agencia->setCiudad($_POST['ciudad']);
+        $agencia->setEstado($_POST['estado']);
+        $agencia->setCP($_POST['cp']);
+        $agencia->setPais($_POST['pais']);
+        $agencia->setMoneda($_POST['moneda']);
+        $agencia->setTel1($_POST['tel1']);
+        $agencia->setTel2($_POST['tel2']);
+        $agencia->setTel3($_POST['tel3']);
+        $agencia->setPaginaWeb($_POST['pagina_web']);
+        $agencia->setActivo('Si');
+        $agencia->setClaveBackOffice($_POST['clave_back_office']);
+        $agencia->setHeaderFooter('Activo');
+        $agencia->setMenu('Activo');
+        //$agencia->setObservaciones($_POST['observaciones']);
+        $agencia->setNombreContacto($_POST['nombre_contacto']);
+        $agencia->setApellidoPaterno($_POST['apellido_paterno']);
+        $agencia->setApellidoMaterno($_POST['apellido_materno']);
+        $agencia->setCargo($_POST['cargo']);
+        $agencia->setSexo($_POST['sexo']);
+        $agencia->setTelDirecto($_POST['tel_directo']);
+        $agencia->setTelMovil($_POST['tel_movil']);
+        $agencia->setEmailContacto($_POST['email_contacto']);
+        $agencia->setEmailServidor($_POST['email_servidor']);
+        $agencia->setClave($_POST['clave']);
+        $agencia->setServidorSMTP($_POST['servidor_smtp']);
+        $agencia->setPortSMTP($_POST['port_smtp']);
+        $agencia->setFechaCreacion(date('Y-m-d'));
+
+
+        $rutaServidor = 'uploads/images';
+        $rutaServidorFiles = 'uploads/files';
+
+        if ($_FILES['url_img1']['name']!=null) {
+    
+          if (!is_dir('uploads/images')) {
+            mkdir('uploads/images', 0777, true); 
+          }
+         
+          $rutaTemporal1 = $_FILES['url_img1']['tmp_name'];
+          $extension = pathinfo($_FILES['url_img1']['name'], PATHINFO_EXTENSION);
+          $nombreImagen1 = date('YmdHis').'_logo.'.$extension;
+          $rutaDestino1 = $rutaServidor.'/'.$nombreImagen1;
+          unlink($_POST['logo']);
+          move_uploaded_file($rutaTemporal1, $rutaDestino1); 
+          $agencia->setLogo($rutaDestino1); 
+        
+         } else{
+          $agencia->setLogo($_POST['logo']);    
+            } 
+
+
+         if ($agencia->guardar()) {
+
+            
+            define("DEMO", false); 
+        
+        
+            $template_file = "../template/email_templates/template_solicitud.php";
+        
+            $email= 'soporte@htop.com.mx';
+
+            $email_from = "Solicitud de registro <".$_POST['email_contacto'].">";
+        
+        
+            $swap_var = array(
+                "{SITE_ADDR}" => "https://www.htop.com.mx",
+                "{EMAIL_TITLE}" => "Ha recibido una solicitud de ".$_POST['nombre_comercial'],
+                "{NOMBRE_COMERCIAL}" => $_POST['nombre_comercial'],
+            );
+        
+        
+            $email_headers = "From: ".$email_from."\r\nReply-To: ".$email_from."\r\n";
+            $email_headers .= "MIME-Version: 1.0\r\n";
+            $email_headers .= "Content-Type: text/html; charset=UTF-8 \r\n";
+        
+        
+            $email_to = $email;
+            $email_subject = $swap_var['{EMAIL_TITLE}']; 
+        
+        
+            if (file_exists($template_file)){
+                $email_message = file_get_contents($template_file);
+            }else{
+                die ("Error al cargar el template");
+            }
+            
+            foreach (array_keys($swap_var) as $key){
+                if (strlen($key) > 2 && trim($swap_var[$key]) != '')
+                    $email_message = str_replace($key, $swap_var[$key], $email_message);
+            }
+        
+            if (DEMO){
+                die("<hr /><center>Esto solo es una prueba </center>");
+        
+            }
+            if (mail($email_to, $email_subject, $email_message, $email_headers) ){ 
+              
+            
+           
+
+            echo '<script>
+            alert("Se Guardó Correctamente");
+            
+            window.location.href="../index.php";
+            </script>';
+        
+        
+          
+          }else{
+          
+
+            //Mensaje si ocurrio un error al enviar mensaje
+          }
+          
+        }
+    }
+           
+        
+        
+
+?>
