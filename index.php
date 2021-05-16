27c8d50b2823087566550f22d8beb2aae1c028c4
ChrisLozano86 <chris.lozano8603@gmail.com>
2021-05-15 22:25:32 -0500

    15/05/2021

--- index.php
+++ index.php
@@ -202,7 +202,7 @@ include_once('lib/registrar_agencia.php');
 
                         <h3 class="text-center">Login</h3>
 
-                        <div class='onesignal-customlink-container'></div>
+                       
                         <form method="post" action="https://online.holidaytravel.com.mx/auth/login">
                             <div class="check-date">
                                 <label for="usuario">Usuario:</label>
