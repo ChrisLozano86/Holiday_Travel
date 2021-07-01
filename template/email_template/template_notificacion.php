5b6fce9535c9ce80a2308bd1e1cca02281731293
ChrisLozano86 <chris.lozano8603@gmail.com>
2021-07-01 17:07:56 -0500

    modulo_pagos

--- template/email_template/template_notificacion.php
+++ template/email_template/template_notificacion.php
@@ -22,23 +22,21 @@
 			
 
 			<p>Se ha detectado que la siguiente reservación con fecha limite de pago para el <strong>{FECHA_LIMITE}</strong>, no ha cubierto la totalidad del pago.</p>
-			<p > Información de la reservación </p>
-
-			<table border="1" cellpadding="2" cellspacing="0">
+            <table border="1" cellpadding="3" cellspacing="0">
                 <tr>
-                    <td style="width: 180px;"><strong>Agencia</strong></td>
+                    <td style="width: 150px;"><strong>Agencia</strong></td>
                     <td>{AGENCIA}</td>
                 </tr>
                 <tr>
-                    <td style="width: 180px;"><strong>Cliente</strong></td>
+                    <td style="width: 150px;"><strong>Cliente</strong></td>
                     <td>{BROKER}</td>
                 </tr>
                 <tr>
-                    <td style="width: 180px;"><strong>Broker</strong></td>
+                    <td style="width: 150px;"><strong>Broker</strong></td>
                     <td>{CLAVE}</td>
                 </tr>
                 <tr>
-                    <td style="width: 180px;"><strong>Clave</strong></td>
+                    <td style="width: 150px;"><strong>Clave</strong></td>
                     <td>{TITULAR}</td>
                 </tr>
             </table>
